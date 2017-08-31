168f07645ecb27d1cf41369b9af1655ce50ac780
Henrique Mayer <henriqmayer@gmail.com>
2017-08-31 04:34:25 -0300

    created models

---
+++ courses/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Tool, Tag, Feedback, Course, Pro, Con
+
+admin.site.register(Tool)
+admin.site.register(Tag)
+admin.site.register(Feedback)
+admin.site.register(Course)
+admin.site.register(Pro)
+admin.site.register(Con)
