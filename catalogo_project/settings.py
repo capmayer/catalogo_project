490fe959270f6d75e37440af7a040a631b4f4ed6
Henrique Mayer <henriqmayer@gmail.com>
2017-10-05 02:41:16 -0300

    debug to true

--- catalogo_project/settings.py
+++ catalogo_project/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
