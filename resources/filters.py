0b361cbd510cd5c2a1b0048ce0d335508fb6b767
Henrique Mayer <henriqmayer@gmail.com>
2017-11-09 05:06:50 -0200

    added full html for feedbacks

--- resources/filters.py
+++ resources/filters.py
@@ -4,8 +4,13 @@ import django_filters
 class ResourceFilter(django_filters.FilterSet):
     class Meta:
         model = Resource
-        fields = ['difficult_education', 'difficult_student',]
+        fields = {
+            'difficult_education':['exact'],
+            'difficult_student':['exact'],
+            'title':['exact', 'icontains']
+        }
         labels = {
             "difficult_education": "Dificuldade para o educador:",
             "difficult_student": "Dificuldade para o estudante:",
+            "title": "Nome: ",
         }
