1d25acad36fe1021cd9c564cb9b7556d62c6fb18
Henrique Mayer <henriqmayer@gmail.com>
2017-11-26 19:16:38 -0200

    fixed resource all temaplte

--- static/js/App.js
+++ static/js/App.js
@@ -4,14 +4,16 @@ import Resource from './components/Resource.vue'
 import Homepage from './components/Homepage.vue'
 import lodash from 'lodash'
 import BootstrapVue from 'bootstrap-vue'
+import Icon from 'vue-awesome'
 
 import { Card, FormRadio } from 'bootstrap-vue/es/components';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-awesome/icons'
 
 Vue.use(VueResource, lodash)
-
+Vue.component('icon', Icon)
 Vue.use(BootstrapVue)
 Vue.use(Card)
 Vue.use(FormRadio)
--- static/js/components/Resource.vue
+++ static/js/components/Resource.vue
@@ -6,28 +6,61 @@
       </div>
       <div class="row">
         <div class="col-md-3">
-            <h1 >Filtros</h1>
-            <hr>
-            <b-form-input type="text" v-model="searchFilter" placeholder="Filtrar pelo nome..." /></b-form-input>
-            <b-form-radio-group id="btnradios1"
+          <div class="row margin-left">
+            <icon name="filter" scale="1.7"></icon><h3 class="margin-left">Filtros</h3>
+          </div>
+          <hr>
+          <b-input-group>
+            <b-input-group-addon><icon name="search"></icon></b-input-group-addon>
+            <b-form-input type="search" v-model="searchFilter" placeholder="Filtrar pelo nome..."/></b-form-input></b-col>
+          </b-input-group>
+          <hr>
+          <h3>Nível de engajamento:</h3>
+          <b-form-radio-group id="btnradios1"
                         buttons
                         v-model="difficultStudentSelected"
                         :options="options"
                         name="radiosBtnDefault" />
+          <hr>
+          <b-input-group>
+            <b-input-group-addon><icon name="globe"></icon></b-input-group-addon>
+            <b-form-select v-model="language" :options="languageOptions">
+            </b-form-select>
+          </b-input-group>
+          <hr>
+          <h3 class="center">Caracteristicas</h3>
+          <b-form-checkbox-group v-model="featuresSelected" stacked :options="featuresOptions">
+          </b-form-checkbox-group>
         </div>
         <div class="col-md-9">
           <b-card-group deck>
             <div v-for="resource in listFilter">
-              <b-card :title=resource.title
-                  :img-src=imageUrl(resource.image_set[0])
-                  img-alt="Image"
-                  img-top rounded height="300"
+              <b-card no-body img-top
                   style="max-width: 40rem;"
                   class="mb-4">
-                <p class="card-text">
-                  {{ resource.description }}
-                </p>
-                <a :href=resourceUrl(resource.slug)><b-button variant="primary">detalhes</b-button></a>
+                <b-img rounded width="400" height="300" :src=imageUrl(resource.image_set[0]) alt="img" />
+
+                <b-card-body>
+                  <h4>{{ resource.title }}</h4>
+                  <p class="card-text">
+                    {{ resource.description }}
+                  </p>
+                  <div class="row">
+                    <div class="col-md-8">
+                      <div class="row">
+                        <icon name="thumbs-o-up" class="margin-15 blue" scale="1.5"></icon>
+                        <h4 class="margin-left margin-right blue">10</h4>
+                        <icon name="thumbs-o-down" class="margin-left red" scale="1.5"></icon>
+                        <h4 class="margin-left margin-right red">10</h4>
+                        <icon name="comment-o" class="margin-left" scale="1.5"></icon>
+                        <h4 class="margin-left">10</h4>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <a :href=resourceUrl(resource.slug)><b-button variant="primary">detalhes</b-button></a>
+                    </div>
+                  </div>
+                </b-card-body>
               </b-card>
             </div>
           </b-card-group>
@@ -46,6 +79,17 @@ export default {
       searchFilter: '',
       listSave: [],
       listFilter: [],
+      language: 'pt-br',
+      languageOptions: [
+        { text: 'Português', value:'pt-br' },
+        { text: 'Inglês', value:'en-us' },
+      ],
+      featuresSelected: [],
+      featuresOptions: [
+        { text: 'Computador', value: 'computador' },
+        { text: 'Projetor', value: 'projetor' },
+        { text: 'Ferramentas', value: 'ferramentas' },
+      ],
       difficultStudentSelected: 'in',
       options: [
         { text: 'Fácil', value: 'in' },
@@ -110,5 +154,26 @@ export default {
 <style>
   .margin-top{
     margin-top: 50px;
+  }
+  .margin-left{
+    margin-left: 5px;
+  }
+  .margin-15{
+    margin-left: 15px;
+  }
+  .margin-right{
+    margin-right: 20px;
+  }
+  .blue{
+    color: #5b9aff;
+  }
+  .red {
+    color: #db0f3b;
+  }
+  .right{
+    text-align: right;
+  }
+  .center {
+    text-align: center;
   }
 </style>
